450b41397af60925c441f4f6ba3dad02cc9af37d
Priyanshu-240499 <97544184+Priyanshu-240499@users.noreply.github.com>
2022-01-16 14:03:50 +0530

    BiggestNumber.java

---
+++ BiggestNumber.java
@@ -0,0 +1,23 @@
+package assignment1;
+
+public class BiggestNumber {
+
+	public static void main(String[] args) {
+		int x1 = 100;
+	    int x2 = 200;
+	    int x3 = 300;
+			if(x1 > x2 ) {
+				System.out.println(x1);
+			}
+				else if(x2 > x3 )
+				{
+					System.out.println(x2);
+					
+				}
+				else if (x3 > x1) {
+					System.out.println(x3);
+				}
+
+	}
+
+}
